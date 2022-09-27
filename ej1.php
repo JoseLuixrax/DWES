b4cd6875e28e516152bc915a6c0c2a51105eb3d7
Jose Luixrax <a21pelajo1@iesgrancapitan.org>
2022-09-27 08:57:52 +0200

    ej1

---
+++ ej1.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @author Jose Luis Pérez Lara
+ * 1. Almacena tres números en variables y escribirlos en pantalla de manera ordenada.
+ */
+$num1 = 1;
+$num2 = 20;
+$num3 = 3;
+
+if ($num1 > $num2 && $num1 > $num3) {
+    echo "$num1,";
+    if ($num2 > $num3) {
+        echo "$num2, $num3";
+    } else {
+        echo "$num3, $num2";
+    }
+}   elseif ($num2 > $num1 && $num2 > $num3) {
+    echo "$num2, ";
+    if ($num1 > $num3) {
+    echo "$num1, $num3";
+    }   else {
+        echo "$num3, $num1";
+    }
+} else {
+    echo "$num3, ";
+    if ($num1 > $num2) {
+        echo "$num1, $num2";
+    } else{
+        echo "$num2, $num1"
+    }
+}
