a904eec92b504b9991debed38b50e4524736e80e
Jose Luixrax <a21pelajo1@iesgrancapitan.org>
2022-09-28 13:28:46 +0200

    ej1

---
+++ bucles/ej1.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @author Jose Luis Pérez Lara
+ * Escribir los números 1 al 10
+ */
+for ($i=1; $i <= 10; $i++) { 
+    echo "$i ";
+}
+echo '</br>Enlace a <a href="https://github.com/JoseLuixrax/DWESUd3/blob/main/bucles/ej1.php" target="_blank">GitHub</a>';
