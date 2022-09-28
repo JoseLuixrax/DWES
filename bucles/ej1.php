72fb9f161343e627cf5ed126f04068839874b1a0
Jose Luixrax <a21pelajo1@iesgrancapitan.org>
2022-09-28 13:18:06 +0200

    ej1 bucles

---
+++ bucles/ej1.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author Jose Luis Pérez Lara
+ * Sumar los 3 primeros números pares.
+*/
+$sum = 0;
+for ($i=0; $i < 6; $i++) { 
+    if ($i % 2 == 0) {
+        $sum += $i;
+    }
+}
+echo "La suma de los 3 primeros números pares es $sum";
+echo '</br>Enlace a <a href="https://github.com/JoseLuixrax/DWESUd3/blob/main/bucles/ej1.php" target="_blank">GitHub</a>';
