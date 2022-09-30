7cd14fe14582829e435df84ee4cc546b15ba32c3
Jose Luixrax <a21pelajo1@iesgrancapitan.org>
2022-09-30 14:14:06 +0200

    ej4

---
+++ array/prueba.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @author Jose Luis Pérez Lara
+ */
+$contactos = array(
+    array("id" => 1, "nombre"=> "Mafalda", "tlfno" => 666123346),
+    array("id" => 2, "nombre"=> "Manolito", "tlfno" => 666123346),
+    array("id" => 3, "nombre"=> "Felipe", "tlfno" => 6661214356),
+);
--- bucles/ej4.php
+++ bucles/ej4.php
@@ -5,18 +5,15 @@
  * Cada celda será un enlace a una página que mostrará un fondo de pantalla con el color seleccionado. 
  * ¿Puedes hacerlo con los conocimientos que tienes?
  */
-$n = 10;
-$m = 10;
-echo '<table border="1">';
-for ($i=1; $i <= $n; $i++) { 
-    echo '<tr>';
-    for ($j=1; $j <= $m; $j++) { 
-        echo '<td>';
-        echo $i * $j;
-        echo '</td>';
+for ($r=0; $r < 255; $r++) { 
+    for ($g=0; $g < 255; $g++) { 
+        for ($b=0; $b < 255; $b++) { 
+            echo '<a href="ej4.php?r='.$r.'&g='.$g.'&b='.$b.'">';
+            echo '<div style="width: 50px; height: 50px; background-color: rgb('.$r.','.$g.','.$b.');"></div>';
+            echo '</a>';
+        }
     }
-    echo '</tr>';
 }
+
 echo '</table>';
 echo '</br>Enlace a <a href="https://github.com/JoseLuixrax/DWESUd3/blob/main/bucles/ej4.php" target="_blank">GitHub</a>';
-
